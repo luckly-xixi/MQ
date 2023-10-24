7111029ccd095dabd64fe1e626cfc31078d42a2d
xixi <1348673083@qq.com>
2023-10-24 23:35:45 +0800

    SQLite引入

---
+++ MQ/src/main/java/com/example/mq/mqserver/core/BasicProperties.java
@@ -0,0 +1,41 @@
+package com.example.mq.mqserver.core;
+
+import java.io.Serializable;
+
+public class BasicProperties implements Serializable {
+
+    // 消息身份的唯一标识,使用UUID
+    private String messageId;
+
+    private String routingKey;
+
+    // 消息是否持久化 ： 1不持久、2持久
+    private int deliverMode = 1;
+
+
+
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+
+    public String getRoutingKey() {
+        return routingKey;
+    }
+
+    public void setRoutingKey(String routingKey) {
+        this.routingKey = routingKey;
+    }
+
+    public int getDeliverMode() {
+        return deliverMode;
+    }
+
+    public void setDeliverMode(int deliverMode) {
+        this.deliverMode = deliverMode;
+    }
+}
--- MQ/src/main/java/com/example/mq/mqserver/core/Message.java
+++ MQ/src/main/java/com/example/mq/mqserver/core/Message.java
@@ -1,8 +1,102 @@
 package com.example.mq.mqserver.core;
 
 
+import java.io.Serializable;
+import java.util.UUID;
+
 /*
-*  表示一个传递的消息
+*  表示一个传递的消息,消息能够在网路上传输，并且需要写入到文件（支持序列化反序列化）
 * */
-public class Message {
+public class Message implements Serializable {
+
+    private BasicProperties basicProperties = new BasicProperties();
+
+    private byte[] body;
+
+//    [offsetBeg,offsetEnd)
+    private transient long offsetBeg = 0; // 消息数据开头距离文件开头位置的偏移量(字节)
+    private transient long offsetEnd = 0; // 消息数据结尾距离文件开头位置的偏移量(字节)
+
+    private byte isValid = 0x1; // 是否有效
+
+    //创建一个工厂方法,用来封装传创建的Message对象
+    public static Message createMessageWithId(String routingKey,BasicProperties basicProperties,byte[] body) {
+        Message message = new Message();
+        if(basicProperties != null) {
+            message.setBasicProperties(basicProperties);
+        }
+        message.setMessageId("M-" + UUID.randomUUID());
+        message.setRoutingKey(routingKey);
+        message.setBody(body);
+        return message;
+    }
+
+
+    public String getMessageId() {
+        return basicProperties.getMessageId();
+    }
+
+    public void setMessageId(String messageId) {
+        basicProperties.setMessageId(messageId);
+    }
+
+    public String getRoutingKey() {
+        return basicProperties.getRoutingKey();
+    }
+
+    public void setRoutingKey(String routingKey) {
+        basicProperties.setRoutingKey(routingKey);
+    }
+
+    public int getDeliverMode() {
+        return basicProperties.getDeliverMode();
+    }
+
+    public void setDeliverMode(int mode) {
+        basicProperties.setDeliverMode(mode);
+    }
+
+
+
+
+
+    public BasicProperties getBasicProperties() {
+        return basicProperties;
+    }
+
+    public void setBasicProperties(BasicProperties basicProperties) {
+        this.basicProperties = basicProperties;
+    }
+
+    public byte[] getBody() {
+        return body;
+    }
+
+    public void setBody(byte[] body) {
+        this.body = body;
+    }
+
+    public long getOffsetBeg() {
+        return offsetBeg;
+    }
+
+    public void setOffsetBeg(long offsetBeg) {
+        this.offsetBeg = offsetBeg;
+    }
+
+    public long getOffsetEnd() {
+        return offsetEnd;
+    }
+
+    public void setOffsetEnd(long offsetEnd) {
+        this.offsetEnd = offsetEnd;
+    }
+
+    public byte getIsValid() {
+        return isValid;
+    }
+
+    public void setIsValid(byte isValid) {
+        this.isValid = isValid;
+    }
 }
